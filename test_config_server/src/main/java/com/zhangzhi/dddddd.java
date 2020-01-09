e53682b7d63385f82bfaad76a5d8ee18c3f9c41a
zhangzhi911 <257444858@qq.com>
2020-01-09 10:08:11 +0800

    config第一次上传
    Signed-off-by: zhangzhi911 <257444858@qq.com>

---
+++ test_config_server/src/main/java/com/zhangzhi/dddddd.java
@@ -0,0 +1,34 @@
+package com.zhangzhi;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableConfigServer
+@RestController
+public class dddddd {
+
+    public static void main(String[] args) {
+        SpringApplication.run(dddddd.class);
+    }
+
+
+    @RequestMapping("/testHelth")
+    public String test(){
+        System.out.println("========testHelth=======");
+        return "ok-helth";
+    }
+//
+//    @Value("${spring.cloud}")
+//    private String config;
+//
+//    @RequestMapping("/test/config")
+//    public String testw() {
+//        return config;
+//    }
+
+}
